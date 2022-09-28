2c47b4797b67a2ff696a555b30645b36b3debd00
unknown <peluffojuancruz@gmail.com>
2022-09-27 23:46:31 -0300

    REMOVE FROM CART

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,27 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import { productListReducer } from './reducers/products.js';
+import {
+  productDetailsReducer,
+  productListReducer,
+} from './reducers/products.js';
+import { cartReducer } from './reducers/cart.js';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromLocalStorage = window.localStorage.getItem('cartItems')
+  ? JSON.parse(window.localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: {
+    cartItems: cartItemsFromLocalStorage,
+  },
+};
 
 const middleware = [thunk];
 
